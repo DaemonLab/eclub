bb8c5d143d53d7bc9e5e0dcfd2913fc67db9ec30
aneeshnema <aneeshnema@gmail.com>
2018-08-18 22:00:39 +0530

    events fixed
    fixed events

--- js/events.js
+++ js/events.js
@@ -4,31 +4,38 @@ Vue.component("event-component", {
       events: [
         {
           id: 0,
-          name: "Project Name",
+          name: "event Name",
           description:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-          image: "img/project/game1.png"
+          image: "img/event/game1.png"
         },
         {
           id: 1,
-          name: "Project Name2",
+          name: "event Name2",
           description:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-          image: "img/project/software1.svg"
+          image: "img/event/software1.svg"
         },
         {
           id: 2,
-          name: "Project Name3",
+          name: "event Name3",
           description:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-          image: "img/project/web1.svg"
+          image: "img/event/web1.svg"
         },
         {
           id: 3,
-          name: "Project Name4",
+          name: "event Name4",
           description:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-          image: "img/project/VueLogo.png"
+          image: "img/event/VueLogo.png"
+        },
+        {
+          id: 4,
+          name: "event Name5",
+          description:
+            "Test Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+          image: "img/event/VueLogo.png"
         }
       ]
     };
@@ -38,7 +45,7 @@ Vue.component("event-component", {
       <div class="col-lg-4 d-flex align-items-stretch" v-for="event in events" :key="event.id">
         <div class="card">
           <div class="profile-container">
-            <img class="card-img-top" :src="project.image" alt="">
+            <img class="card-img-top" :src="event.image" alt="">
           </div>
           <div class="card-body">
             <h3 class="card-title">{{event.name}}</h3>
