865bef246613b1bab01b0bff59b41998036df858
Deep1Origins <shavezmalick@gmail.com>
2018-08-22 16:28:30 +0530

    Updated Project, Events. Fixed Home: made img paths relative

--- js/events.js
+++ js/events.js
@@ -4,39 +4,46 @@ Vue.component("event-component", {
       events: [
         {
           id: 0,
-          name: "event Name",
+          name: "Workshop on Beginner to Advanced Computer Vision Based Sensing",
           description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-          image: "img/event/game1.png"
+            "A 14-day workshop in the summer vacation on the topic 'Beginner to Advanced Computer Vision Based Sensing'.Duration: 8th July - 22nd July 2018 ( 14 Days ).Colour identification. Workshop includes theory and practice sessions",
+          image: "img/project/ComputerVision1.png"
         },
         {
           id: 1,
-          name: "event Name2",
+          name: "Basic Electronics Workshop",
           description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-          image: "img/event/software1.svg"
+            " This Workshop Aims at getting everyone Started with Electronics, equip everyone with the Basic Electronics Knowledge. The workshop is designed to present to you a good blend of Theoretical and Practical understanding with an extensive focus on Qualitative Understanding of the Concepts.This Workshop is divided into Multiple Modules so as to be convenient for you to easily choose only your topics of interest. You can choose to attend the Hands-On Sessions, along with the Theory Sessions",
+          image: "img/event/BasicElectronicsWorkshop.png"
         },
         {
           id: 2,
-          name: "event Name3",
+          name: "Make-a-thon 2k18",
           description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-          image: "img/event/web1.svg"
+            "Presenting to you the one of a kind Hackathon code-named Make-a-thon, Where you will need to Harness every bit of Electronics Skills in you. Only the purest form of Electronics. All the Components will be made available to you. You can participate in Teams of 3. All teams are requested to kindly fill the google form so we can make necessary arrangements.",
+          image: "img/event/Make-a-thon.png"
         },
         {
           id: 3,
-          name: "event Name4",
+          name: "Electrovanza 2.0",
           description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-          image: "img/event/VueLogo.png"
+            "Presenting the Electrovanza 2.0, an event which is designed to give the best possible exposure of fascinating side of electronics to all tech-maniacs, scheduled to happen on this Friday. Event comprises of Arduino Uno workshop with working demonstration of different sensors followed by in depth explanation of each model.",
+          image: "img/event/Electrovanza-2.png"
         },
         {
           id: 4,
-          name: "event Name5",
+          name: "Raspberri Pi Workshop",
           description:
-            "Test Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-          image: "img/event/VueLogo.png"
-        }
+            "We at the Electronics Club want to take your learning a step ahead and closer to successfully making smart products and learn more about microcontrollers by conducting an exciting workshop on Raspberry Pi. The microcontroller is one of the finest available in the market and with its superior computing powers it is quite popular among the hobbyists and product developers alike. The workshop will cover the basics of installing and getting started with the microcontroller coding.",
+          image: "img/event/RaspberriPiWorkshop.png"
+        },
+        {
+          id: 5,
+          name: "Monopoly of Circuits",
+          description:
+            "An event held in Fluxus with prizes worth 15k, to test your Knowledge in electronics",
+          image: "img/event/MonopolyOfCircuits.png"
+        },
       ]
     };
   },
@@ -48,8 +55,12 @@ Vue.component("event-component", {
             <img class="card-img-top" :src="event.image" alt="">
           </div>
           <div class="card-body">
+          <div class="card-body align-items-bottom d-flex justify-content-center">
             <h3 class="card-title">{{event.name}}</h3>
+          </div>
+          <div class="card-body align-items-bottom d-flex justify-content-center">
             <p class="card-text mb-2">{{event.description}}</p>
+          </div>
             <a href="#"><i class="fab fa-github"></i> </a>
           </div>
         </div>
--- js/project.js
+++ js/project.js
@@ -4,39 +4,47 @@ Vue.component("project-component", {
       projects: [
         {
           id: 0,
-          name: "Project Name",
+          name: "Ground Station for Satellite Communication",
           description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-          image: "img/project/game1.png"
+            "To build an on-ground terminal linked to a satellite by an antenna and the associated electronic equipment for the purpose of transmitting and receiving messages, tracking, and control.Ground Station Team for successfully receiving and decoding real-time Images of The Earth from passing by Weather Satellites",
+          image: "img/project/GroundStationNOAAdata1.png"
         },
         {
           id: 1,
-          name: "Project Name2",
+          name: "RFID",
           description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-          image: "img/project/software1.svg"
+            "Crux of the project is to use Arduino to create a system to manage, register and edit the student database in mess and many other places where the entry of students is to be registered by your smart card. There are no particular prerequisites for this project, however knowledge of basic electronics stuff or about database management will sure be handy. Through this project you can learn a great deal about Arduino and its integration with other sensors like RFID reader modules, also you will learn about database creation and management. It is fully funded by Electronics Club, IIT Indore. Link for RFID. This will save time and will shorten queue to great extent.",
+          image: "img/project/RFID.png"
         },
         {
           id: 2,
-          name: "Project Name3",
+          name: "Home Automation",
           description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-          image: "img/project/web1.svg"
+            "As you all are already familiar with home automation, which basically deals with Automating simple stuff which makes life easier. As we will be implementing it in our hostels so the basic things which we can do will be automating lights, fans in rooms as well as common areas. This is going to be an IoT Based Project, We would be controlling the state of the appliance using our mobile phones via Bluetooth or the Internet. Do come up with innovative ideas that are Useful, as well as feasible to implement in our Hostels.",
+          image: "img/project/HomeAutomation.png"
         },
+        
         {
           id: 3,
-          name: "Project Name4",
+          name: "LaunchPad",
           description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-          image: "img/project/VueLogo.png"
+            "This project is about making an Arduino based LaunchPad which can send MIDI notes to our laptor.Further implementation can be using Bluetooth Low EnergyTM to directly send data to bluetooth devices like speaker or headset. There are no perticular prerequisites for this project but some past experience with basic electronics stuff will help you to learn fast. You will learn about Arduino and get familiar with different sensors, MIDI, I2C and SPI communication. It is fully funded by Electronics Club",
+          image: "img/project/LaunchPad.png"
         },
         {
           id: 4,
-          name: "Project Name5",
+          name: "POV",
           description:
-            "Test Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-          image: "img/project/VueLogo.png"
-        }
+            "Persistence Of Vision. Persistence of vision refers to the optical illusion that occurs when visual perception of an object does not cease for some time after the rays of light proceeding from it have ceased to enter the eye. In this project we are going to use POV concept to display words using revolving LEDs. There are no prerequisite for this project. We will learn required things along the way",
+          image: "img/project/POV.png"
+        },
+        {
+          id: 5,
+          name: "Smart Alarm Clock",
+          description:
+            "Alarm Clock with features as Calender Sync and Voice assist. Future development of this project is to integrate Natural Language Processing for interactive purposes",
+          image: "img/project/SmartAlarmClock.png"
+        },
       ]
     };
   },
@@ -48,8 +56,12 @@ Vue.component("project-component", {
             <img class="card-img-top" :src="project.image" alt="">
           </div>
           <div class="card-body">
+          <div class="card-body align-items-bottom d-flex justify-content-center">
             <h3 class="card-title">{{project.name}}</h3>
+          </div>  
+          <div class="card-body align-items-bottom d-flex justify-content-center">
             <p class="card-text mb-2">{{project.description}}</p>
+          </div>
             <a href="#"><i class="fab fa-github"></i> </a>
           </div>
         </div>
