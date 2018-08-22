f8089c27fae7a516ab9e8bafc108561b9028cc60
Deep1Origins <shavezmalick@gmail.com>
2018-08-22 17:51:28 +0530

    Added to Home page

--- js/home.js
+++ js/home.js
@@ -1,7 +1,7 @@
 new Vue({
   el: "#blogposts",
   data: {
-    link: ["index.html", "events.html","team.html", "projects.html"]
+    link: ["index.html", "events.html","team.html", "projects.html","https://medium.com/elecclub-iit-indore/a-brief-introduction-to-computer-vision-technology-fdf0583342e2"]
   },
   methods: {
     openblog(num) {
