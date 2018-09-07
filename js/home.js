7d5bd37f634d90463675a1177052314d0b04c73d
Mrigank Krishan <mrigankkrishan@gmail.com>
2018-09-07 23:06:13 +0530

    pokemon is not an anime

--- js/home.js
+++ js/home.js
@@ -1,11 +1,33 @@
 new Vue({
   el: "#blogposts",
   data: {
-    link: ["index.html", "events.html","team.html", "projects.html","https://medium.com/elecclub-iit-indore/a-brief-introduction-to-computer-vision-technology-fdf0583342e2"]
+    link: ["index.html", "team.html", "projects.html"],
+    posts: []
   },
   methods: {
     openblog(num) {
       window.open(this.link[num]);
     }
+  },
+  created: function () {
+    console.log("hiii, i'm loaded o/");
+    const self = this;
+    axios.get("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Felecclub-iit-indore")
+      .then(function (response) {
+        // handle success
+        self.posts = response.data.items.map((item) => {
+          let desc = item.description;
+          item.guid = item.guid.split("/")[4];
+          item.imageLink = new RegExp('<figure><img alt="" src="(.*?)">.*</figure>', 'g').exec(item.description)[1];
+          let title = item.title;
+          let tmp = desc.split("</figure>");
+          tmp.splice(0, 1);
+          let new_desc = tmp.join("");
+          console.log(tmp);
+          item.description = new_desc;
+          return item;
+        }).splice(0, 3);
+        console.log(response.data.items);
+      })
   }
 });
